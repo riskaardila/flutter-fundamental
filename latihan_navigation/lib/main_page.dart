42aea8f5ed5929fbef45e516508a490a3a4bb89d
riskaardila <riskaardila35@gmail.com>
2021-04-11 20:15:49 +0700

    latihan navigation

---
+++ latihan_navigation/lib/main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Main Page"),),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go to Second Page"),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
